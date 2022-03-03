87a282e823ef7e3989725a75e35c5ac4f1c12552
AntonKisel1 <v-a-t-n-i-k@bk.ru>
2022-03-03 12:19:34 +0500

    Падают все тесты по Group Что то не так с driver.Navigate().GoToUrl

---
+++ addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "fff";
+
+            app.Groups.Create(group);
+            app.Auth.Logout();
+        }
+
+        [Test]
+        public void EmptyGroupCreationTest()
+        {
+            GroupData group = new GroupData("");
+            group.Header = "";
+            group.Footer = "";
+            
+            app.Groups.Create(group);
+            app.Auth.Logout();
+        }
+    }
+}
